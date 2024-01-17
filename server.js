5cc6cabd6c614bc9e07f2c75b709e0b748d7f6c2
Ivan Sheremeta <sheremetavanya2004@gmail.com>
2024-01-17 14:36:24 +0200

    add code to github

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+const app = require('./app');
+
+mongoose.set('strictQuery', true);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(console.log('DB connection successful!'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
