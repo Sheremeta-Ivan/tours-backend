bd9b331651617ce9e56a590eafd4096648d99aab
Ivan Sheremeta <sheremetavanya2004@gmail.com>
2024-01-19 13:15:46 +0200

    add-fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,6 +23,15 @@ const getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 4) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      // Default fields
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
