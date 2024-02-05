a6a5e9c02909b9643c3b7bc8b0895f820087bc6e
Ivan Sheremeta <sheremetavanya2004@gmail.com>
2024-02-05 17:24:46 +0200

    add-error-handling

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 mongoose.set('strictQuery', true);
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_PASSWORD
 );
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(console.log('DB connection successful!'))
-  .catch((err) => {
-    console.log(err);
-  });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}...`);
-});
+mongoose
+  .connect(DB)
+  .then(
+    app.listen(3000, () => {
+      console.log(`Server running on port ${port}...`);
+    }),
+    console.log('DB connection successful!')
+  )
+  .catch((err) => {
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+
+
+
